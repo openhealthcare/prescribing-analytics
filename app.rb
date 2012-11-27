f0075c1a96fabbfd3c6a9f39bbf5c2cae6e87cec
Ross Jones <rossdjones@gmail.com>
2012-11-27 09:07:15 +0000

    Adding empty views

--- app.rb
+++ app.rb
@@ -5,3 +5,27 @@ get '/' do
 end
 
 
+get '/analysis' do
+  erb :index
+end
+
+get '/primer' do
+  erb :index
+end
+
+get '/wasteful' do
+  erb :index
+end
+
+get '/future' do
+  erb :index
+end
+
+get '/contact' do
+  erb :index
+end
+
+get '/about' do
+  erb :index
+end
+
